aeb0b14e16b56cfdec4dafde4aca9e80d2dc8dae
Mina Medhat <minamedhat123456@gmail.com>
2024-09-16 23:53:59 +0300

    UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            ColumnName = new DataGridViewTextBoxColumn();
+            DataType = new DataGridViewComboBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(123, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Table Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(318, 49);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(173, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(123, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Number of Rows";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            numericUpDown1.Location = new Point(318, 119);
+            numericUpDown1.Margin = new Padding(3, 4, 3, 4);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(174, 34);
+            numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(123, 223);
+            label3.Name = "label3";
+            label3.Size = new Size(188, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Column Name & Type";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ColumnName, DataType });
+            dataGridView1.Location = new Point(318, 199);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(303, 154);
+            dataGridView1.TabIndex = 5;
+            // 
+            // ColumnName
+            // 
+            ColumnName.HeaderText = "ColumnName";
+            ColumnName.MinimumWidth = 6;
+            ColumnName.Name = "ColumnName";
+            ColumnName.Width = 125;
+            // 
+            // DataType
+            // 
+            DataType.HeaderText = "DataType";
+            DataType.Items.AddRange(new object[] { "int", "varchar", "float", "datetime", "bit", "decimal" });
+            DataType.MinimumWidth = 6;
+            DataType.Name = "DataType";
+            DataType.Resizable = DataGridViewTriState.True;
+            DataType.SortMode = DataGridViewColumnSortMode.Automatic;
+            DataType.Width = 125;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(768, 479);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 45);
+            button1.TabIndex = 6;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private DataGridViewTextBoxColumn ColumnName;
+        private DataGridViewComboBoxColumn DataType;
     }
+
 }
